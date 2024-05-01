9d4651fa09a61e2fef08b756039fb8cde6d15f68
skn437 <skn437physx@gmail.com>
2024-05-01 13:48:54 +0600

    Updated Dependencies & Fixed README.md

--- src/main/java/best/skn/security/services/impls/package-info.java
+++ src/main/java/best/skn/security/services/impls/package-info.java
@@ -2,7 +2,7 @@
  * Security Service Implementation Package
  *
  * @author SKN Shukhan
- * @version 1.2.0
+ * @version 1.2.1
  * @since 2024-04-30
  * @use.case Spring Boot Reactive
  * @dedicated.to Logno, Atoshi and My Parents
